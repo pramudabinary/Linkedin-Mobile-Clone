992834cc6ce34df21c68b8f3c62a87411df69770
Pramuda Liyanage <pramudatharika@gmail.com>
2021-11-23 21:32:24 +0530

    Initialize Authentication Screen

---
+++ screens/AuthenticationScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import 'react-native-gesture-handler';
+
+const  AuthenticationScreen = () => {
+    return (
+        <View>
+            <Text>Hello</Text>
+        </View>
+    )
+}
+
+export default AuthenticationScreen;

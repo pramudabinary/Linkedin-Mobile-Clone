f9782289101d24cfb07967456885bd9a0d0820df
Pramuda Liyanage <pramudatharika@gmail.com>
2021-11-24 22:47:56 +0530

    Image silder add

--- screens/AuthenticationScreen.js
+++ screens/AuthenticationScreen.js
@@ -1,13 +1,43 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, StyleSheet,Image } from 'react-native'
 import 'react-native-gesture-handler';
+import Carousel from '../components/Carousel'
+import { dummyData } from '../data/Data'
 
 const  AuthenticationScreen = () => {
     return (
-        <View>
-            <Text>Hello</Text>
+        <View style={styles.container}>
+
+                <View style={styles.imageL} >
+                <Image
+        style={styles.tinyLogo}
+        source={require('../assets/linkedin-logo.png')}
+      />
+                </View>
+
+                <View >
+                <Carousel data = {dummyData}/>
+                </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 20,
+      backgroundColor: "#eaeaea"
+    },
+    imageL:{
+        width:20,
+        height:20,
+    },
+    tinyLogo:{
+      // marginBottom:100,
+        width:150,
+        height:50,
+    }
+  });
+  
 
 export default AuthenticationScreen;
